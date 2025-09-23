53c209f806ca028fa6322351796eeed6f0d570e2
DaoChaShao <27882272@qq.com>
2025-09-23 16:16:11 +0800

    feat: update layout to include Model Testing and Real-time Prediction pages

--- utils/layout.py
+++ utils/layout.py
@@ -30,21 +30,24 @@ def pages_setter() -> None:
             "subpages/home.py",
             "subpages/preparation.py",
             "subpages/train.py",
-            # "subpages/test.py",
+            "subpages/test.py",
+            "subpages/realtime.py",
             "subpages/about.py",
         ],
         "title": [
             "Home",
             "Data Preparation",
             "Model Training",
-            # "Model Testing",
+            "Model Testing",
+            "Real-time Prediction",
             "About",
         ],
         "icon": [
             ":material/home:",
             ":material/dataset:",
             ":material/function:",
-            # ":material/assignment:",
+            ":material/assignment:",
+            ":material/functions:",
             ":material/info:",
         ],
     }
@@ -56,9 +59,11 @@ def pages_setter() -> None:
         "Core Functions": [
             Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
             Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
+            Page(page=pages["page"][4], title=pages["title"][4], icon=pages["icon"][4]),
         ],
         "Information": [
-            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
+            Page(page=pages["page"][5], title=pages["title"][5], icon=pages["icon"][5]),
         ],
     }
     pg = navigation(structure, position="sidebar", expanded=True)
